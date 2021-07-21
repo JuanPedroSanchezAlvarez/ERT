22c810ffe61118278fa1074e0af65bd58aa6803b
JuanPedroSanchezAlvarez <juanpedrosanchez1989@gmail.com>
2021-07-21 08:39:47 +0200

    Initial commit

---
+++ ert/src/main/java/com/ert/ErtApplication.java
@@ -0,0 +1,13 @@
+package com.ert;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ErtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ErtApplication.class, args);
+	}
+
+}
---
+++ ert/src/test/java/com/ert/ErtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ert;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ErtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
