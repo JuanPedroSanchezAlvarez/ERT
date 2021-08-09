6709f4c824533ec050cf8d541007e9b4b34669dc
JuanPedroSanchezAlvarez <juanpedrosanchez1989@gmail.com>
2021-08-09 13:08:21 +0200

    Navbar and footer.

--- ert/src/main/java/com/ert/controller/IndexController.java
+++ ert/src/main/java/com/ert/controller/IndexController.java
@@ -1,15 +1,14 @@
 package com.ert.controller;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-
+import org.springframework.web.bind.annotation.GetMapping;
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
 @Controller
 public class IndexController {
 
-	@RequestMapping(path = {"", "/", "index", "index.html"})
+	@GetMapping(path = {"", "/", "index", "index.html"})
 	public String getIndex() {
 		log.debug("LOG: " + this.getClass().getName() + " --> getIndex");
 		return "index";
