02d0159265f11d58af144bbb7da6296cc8331ba6
JuanPedroSanchezAlvarez <juanpedrosanchez1989@gmail.com>
2021-08-10 09:06:46 +0200

    LoggingUtils Class.

--- ert/src/main/java/com/ert/controller/IndexController.java
+++ ert/src/main/java/com/ert/controller/IndexController.java
@@ -2,6 +2,9 @@ package com.ert.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import com.ert.util.LoggingUtils;
+
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
@@ -10,7 +13,7 @@ public class IndexController {
 
 	@GetMapping(path = {"", "/", "index", "index.html"})
 	public String getIndex() {
-		log.debug("LOG: " + this.getClass().getName() + " --> getIndex");
+		log.debug("LOG: " + this.getClass().getName() + " --> " + LoggingUtils.getCurrentMethodName());
 		return "index";
 	}
 	
