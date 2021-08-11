9a4f4417ee4f9441b421b739c5ec6bd47f320071
JuanPedroSanchezAlvarez <juanpedrosanchez1989@gmail.com>
2021-08-11 21:37:56 +0200

    Adding kits and races templates.

--- ert/src/main/java/com/ert/controller/RaceController.java
+++ ert/src/main/java/com/ert/controller/RaceController.java
@@ -2,18 +2,20 @@ package com.ert.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.ert.util.LoggingUtils;
 
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
+@RequestMapping(path = "/races")
 @Controller
-public class IndexController {
+public class RaceController {
 
-	private static final String INDEX = "index";
+	private static final String INDEX = "races/index";
 	
-	@GetMapping(path = {"", "/", "index", "index.html"})
+	@GetMapping(path = {"", "/"})
 	public String getIndex() {
 		log.debug("LOG: Class: " + this.getClass().getName() + " --> Method: " + LoggingUtils.getCurrentMethodName());
 		return INDEX;
